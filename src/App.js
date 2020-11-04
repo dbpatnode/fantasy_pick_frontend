7985ac63b723032eab0e307c2c06ffd31789dae8
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-11-04 22:35:20 +0200

    create new file for standings table

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { setUser, fetchStandings, setLogout } from "./actions";
 import { useMediaQuery } from "react-responsive";
-import { loss, draw, won } from "./services/svg-icons";
 import api from "./services/api";
-import { Collapse, Card, Button, CardBody } from "reactstrap";
+import { Collapse, Button } from "reactstrap";
+import StandingsTable from "./components/standings/StandingsTable";
 
 const Desktop = ({ children }) => {
   const isDesktop = useMediaQuery({ minWidth: 992 });
@@ -32,6 +32,7 @@ class App extends Component {
   componentDidMount() {
     this.props.fetchStandings();
   }
+
   handleLogin = (e, user) => {
     e.preventDefault();
     api.auth
@@ -89,32 +90,31 @@ class App extends Component {
     console.log(this.state);
     return (
       <div className="App">
-        {/* <header className="App-header"> */}
         <div>
           <Desktop>
             <div className="page-container">
-              <header class="header">My header</header>
-              <div className="wrapper">
-                {!this.props.token ? (
-                  <>
-                    <span>
-                      <Button onClick={this.toggleLogin}>Login</Button>
-                      <Collapse isOpen={this.state.loginOpen}>
-                        <Login handleLogin={this.handleLogin} />
-                      </Collapse>{" "}
-                    </span>
-
-                    <span>
-                      <Button onClick={this.toggleSignUp}>Signup</Button>
-                      <Collapse isOpen={this.state.signUpOpen}>
-                        <Signup handleSignUp={this.handleSignUp} />
-                      </Collapse>{" "}
-                    </span>
-                  </>
-                ) : (
-                  <Logout handleLogout={this.handleLogout} />
-                )}
-              </div>
+              <nav>
+                <ul className="nav-links">
+                  {!this.props.token ? (
+                    <>
+                      <li>
+                        <Button onClick={this.toggleLogin}>Login</Button>
+                        <Collapse isOpen={this.state.loginOpen}>
+                          <Login handleLogin={this.handleLogin} />
+                        </Collapse>{" "}
+                      </li>
+                      <li>
+                        <Button onClick={this.toggleSignUp}>Signup</Button>
+                        <Collapse isOpen={this.state.signUpOpen}>
+                          <Signup handleSignUp={this.handleSignUp} />
+                        </Collapse>{" "}
+                      </li>
+                    </>
+                  ) : (
+                    <Logout handleLogout={this.handleLogout} />
+                  )}
+                </ul>
+              </nav>
 
               {/* <article class="content">
                 <h1>2 column, header and footer</h1>
@@ -131,61 +131,7 @@ class App extends Component {
                 </p>
               </article> */}
 
-              <div className="standings-table">
-                <table className="standings-D">
-                  <thead>
-                    <tr>
-                      <th></th>
-                      <th id="standings-club-D">Club</th>
-                      <th>Matches</th>
-                      <th>W</th>
-                      <th>D</th>
-                      <th>L</th>
-                      <th>Points</th>
-                      <th>Last 5</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {this.props.standings[0]
-                      ? this.props.standings[0].map((table) => (
-                          <tr className="standings-row-D">
-                            <td id="standings-crest-td-D">
-                              <span className="standings-position-D">
-                                {table.position}
-                              </span>
-                              <img
-                                src={table.team.crestUrl}
-                                alt="team crest"
-                                className="standings-crest-D"
-                              ></img>
-                            </td>
-                            <td id="standings-club-D">{table.team.name}</td>
-                            <td>{table.playedGames}</td>
-                            <td>{table.won}</td>
-                            <td>{table.draw}</td>
-                            <td>{table.lost}</td>
-                            <td>{table.points}</td>
-                            <td>
-                              {table.form.split(",").map((game) => (
-                                <span>
-                                  {game == "W" ? (
-                                    <span className="Won">{won}</span>
-                                  ) : null}
-                                  {game == "L" ? (
-                                    <span className="Loss">{loss}</span>
-                                  ) : null}
-                                  {game == "D" ? (
-                                    <span className="Draw">{draw}</span>
-                                  ) : null}
-                                </span>
-                              ))}
-                            </td>
-                          </tr>
-                        ))
-                      : null}
-                  </tbody>
-                </table>
-              </div>
+              <StandingsTable standings={this.props.standings} />
             </div>
           </Desktop>
           <Tablet>Tablet</Tablet>
@@ -205,9 +151,9 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
   // actions.js
   return {
-    fetchStandings: () => dispatch(fetchStandings()),
     setUser: (user) => dispatch(setUser(user)),
     setLogout: () => dispatch(setLogout()),
+    fetchStandings: () => dispatch(fetchStandings()),
   };
 }
 
---
+++ src/components/standings/StandingsTable.js
@@ -0,0 +1,63 @@
+import { loss, draw, won } from "../../services/svg-icons";
+
+const StandingsTable = (props) => {
+  console.log(props.standings);
+  return (
+    <div className="standings-table">
+      <table className="standings-D">
+        <thead>
+          <tr>
+            <th></th>
+            <th id="standings-club-D">Club</th>
+            <th>Matches</th>
+            <th>W</th>
+            <th>D</th>
+            <th>L</th>
+            <th>Points</th>
+            <th>Last 5</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.standings[0]
+            ? props.standings[0].map((table) => (
+                <tr className="standings-row-D">
+                  <td id="standings-crest-td-D">
+                    <span className="standings-position-D">
+                      {table.position}
+                    </span>
+                    <img
+                      src={table.team.crestUrl}
+                      alt="team crest"
+                      className="standings-crest-D"
+                    ></img>
+                  </td>
+                  <td id="standings-club-D">{table.team.name}</td>
+                  <td>{table.playedGames}</td>
+                  <td>{table.won}</td>
+                  <td>{table.draw}</td>
+                  <td>{table.lost}</td>
+                  <td>{table.points}</td>
+                  <td>
+                    {table.form.split(",").map((game) => (
+                      <span>
+                        {game == "W" ? (
+                          <span className="Won">{won}</span>
+                        ) : null}
+                        {game == "L" ? (
+                          <span className="Loss">{loss}</span>
+                        ) : null}
+                        {game == "D" ? (
+                          <span className="Draw">{draw}</span>
+                        ) : null}
+                      </span>
+                    ))}
+                  </td>
+                </tr>
+              ))
+            : null}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+export default StandingsTable;
