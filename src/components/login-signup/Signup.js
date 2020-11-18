0a5a92ba58e8c7ef733c54f1d3da4a646c1ce98b
LucasLeibs <dr.lucasleibs@gmail.com>
2020-11-17 19:21:44 -0800

    added semantic ui to signup modal but is unfinished

--- src/components/login-signup/Signup.js
+++ src/components/login-signup/Signup.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ModalBody } from "reactstrap";
 import { capitalize } from "../../services/helpers";
+import { send, user, password } from "../../services/svg-icons";
 
 class Signup extends React.Component {
   state = {
@@ -19,29 +20,38 @@ class Signup extends React.Component {
     return (
       <>
         <ModalBody>
-          <form onSubmit={(e) => this.props.handleSignUp(e, this.state)}>
-            <input
+          <form  onSubmit={(e) => this.props.handleSignUp(e, this.state)}>
+           
+              <span>{user}</span>
+            <input className="ui label"
+            id= "username-input"
               type="text"
               onChange={this.handleChange}
               value={this.state.username}
               name="username"
               placeholder="username"
             ></input>
-            <input
+        
+          
+            <input className="ui label"
+              id="email-input"
               type="text"
               onChange={this.handleChange}
               name="email"
               placeholder="Email"
               value={this.state.email}
             />
-            <input
+           
+            <input className="ui label"
+            id="password-input"
               type="password"
               onChange={this.handleChange}
               value={this.state.password}
               name="password"
               placeholder="Password"
             ></input>
-            <input
+            <input className="ui label"
+          id ="password-c-input"
               type="password"
               onChange={this.handleChange}
               name="password_confirmation"
