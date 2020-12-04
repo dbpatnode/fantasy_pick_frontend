ce9651222ebee20896f13fc53cc0629183251021
dbpatnodee <dbpatnode@hotmail.com>
2020-12-04 13:50:44 -0800

    added spread opperator to messages

--- src/components/contexts/ConversationsProvider.js
+++ src/components/contexts/ConversationsProvider.js
@@ -4,18 +4,16 @@ import useLocalStorage from "../hooks/useLocalStorage";
 const ConversationsContext = React.createContext();
 
 export function useConversations() {
-
   return useContext(ConversationsContext);
 }
 
 export function ConversationsProvider({ children, league, user }) {
   const [conversations, setConversations] = useLocalStorage(
     "conversations",
-    []);
-  const [selectedConversationIndex, setSelectedConversationIndex] = useState(0)
+    []
+  );
+  const [selectedConversationIndex, setSelectedConversationIndex] = useState(0);
   const users = league.join.map((join) => join.user);
-  
-
 
   function createConversation(recipients) {
     setConversations((prevConversations) => {
@@ -23,76 +21,75 @@ export function ConversationsProvider({ children, league, user }) {
     });
   }
 
-  function addMessageToConversation({ recipients, text, sender}) {
-    setConversations(prevConversations => {
-      let madeChange = false
-      const newMessage = { sender, text}
-      const newConversations = prevConversations.map(conversation => {
-        if(arrayEquality(conversation.recipients, recipients)) {
-          madeChange = true
-          return {...conversation, messages: [conversation.messages, newMessage]}
+  function addMessageToConversation({ recipients, text, sender }) {
+    setConversations((prevConversations) => {
+      let madeChange = false;
+      const newMessage = { sender, text };
+      const newConversations = prevConversations.map((conversation) => {
+        if (arrayEquality(conversation.recipients, recipients)) {
+          madeChange = true;
+          return {
+            ...conversation,
+            messages: [...conversation.messages, newMessage],
+          };
         }
 
-        return conversation 
-      })
+        return conversation;
+      });
       if (madeChange) {
-        return newConversations
+        return newConversations;
       } else {
-        return [...prevConversations, {recipients, messages: [newMessage]}]
+        return [...prevConversations, { recipients, messages: [newMessage] }];
       }
-    })
+    });
   }
 
   function sendMessage(recipients, text) {
-    console.log(user)
-    addMessageToConversation({ recipients, text, sender: user.id})
+    console.log(user);
+    addMessageToConversation({ recipients, text, sender: user.id });
   }
 
-
-const formattedConversations = conversations.map((conversation, index) => {
-  const recipients = conversation.recipients.map(recipient => {
-    const member = users.find(user => {
-      return user.id === recipient
-    })
-    const name = (member && member.username) || recipient
-    return { id: recipient, name}
-  })
-  const messages = conversation.messages.map(message => {
-    const member = users.find(user => {
-      return user.id === message.sender
-  })
-  const name = (member && member.username) || message.sender
-  const fromMe = user.id === message.sender
-  return {...message, senderName: name, fromMe}
-})
-  const selected = index === selectedConversationIndex
-  return { ...conversation, messages, recipients, selected}
-})
-const value = {
-  conversations: formattedConversations, 
-  createConversation,
-  sendMessage,
-  selectedConversationIndex: setSelectedConversationIndex, 
-  selectedConversation: formattedConversations[selectedConversationIndex]
-}
+  const formattedConversations = conversations.map((conversation, index) => {
+    const recipients = conversation.recipients.map((recipient) => {
+      const member = users.find((user) => {
+        return user.id === recipient;
+      });
+      const name = (member && member.username) || recipient;
+      return { id: recipient, name };
+    });
+    const messages = conversation.messages.map((message) => {
+      const member = users.find((user) => {
+        return user.id === message.sender;
+      });
+      const name = (member && member.username) || message.sender;
+      const fromMe = user.id === message.sender;
+      return { ...message, senderName: name, fromMe };
+    });
+    const selected = index === selectedConversationIndex;
+    return { ...conversation, messages, recipients, selected };
+  });
+  const value = {
+    conversations: formattedConversations,
+    createConversation,
+    sendMessage,
+    selectedConversationIndex: setSelectedConversationIndex,
+    selectedConversation: formattedConversations[selectedConversationIndex],
+  };
 
   return (
-    <ConversationsContext.Provider
-      value={value}
-    >
+    <ConversationsContext.Provider value={value}>
       {children}
     </ConversationsContext.Provider>
   );
 }
 
+function arrayEquality(a, b) {
+  if (a.length !== b.length) return false;
 
-function arrayEquality(a,b) {
-  if (a.length !== b.length) return false
-
-  a.sort()
-  b.sort()
+  a.sort();
+  b.sort();
 
   return a.every((element, index) => {
-    return element === b[index]
-  })
+    return element === b[index];
+  });
 }
