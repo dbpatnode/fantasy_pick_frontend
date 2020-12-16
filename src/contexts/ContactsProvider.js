1d490152ae559336d7f92d7a86d9902419f4c4ac
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-12-16 17:59:34 +0200

    user cannot add same id as a new contact

--- src/contexts/ContactsProvider.js
+++ src/contexts/ContactsProvider.js
@@ -1,24 +1,31 @@
-import React, { useContext } from 'react'
-import useLocalStorage from '../hooks/useLocalStorage';
+import React, { useContext, useState } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
 
-const ContactsContext = React.createContext()
+const ContactsContext = React.createContext();
 
 export function useContacts() {
-  return useContext(ContactsContext)
+  return useContext(ContactsContext);
 }
 
 export function ContactsProvider({ children }) {
-  const [contacts, setContacts] = useLocalStorage('contacts', [])
+  const [contacts, setContacts] = useLocalStorage("contacts", []);
 
   function createContact(id, name) {
-    setContacts(prevContacts => {
-      return [...prevContacts, { id, name }]
-    })
+    setContacts((prevContacts) => {
+      let contact = prevContacts.find((c) => c.id === id);
+
+      if (contact) {
+        alert(`This Id is already in your contact list as ${contact.name}`);
+        return prevContacts;
+      } else {
+        return [...prevContacts, { id, name }];
+      }
+    });
   }
 
   return (
     <ContactsContext.Provider value={{ contacts, createContact }}>
       {children}
     </ContactsContext.Provider>
-  )
+  );
 }
